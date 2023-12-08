fe959e2038c9db6180e722f9fc4fc074852cd1fe
Jiale <114423476+JialeHe28@users.noreply.github.com>
2023-12-08 20:36:29 +0100

    Progreso

--- src/game.js
+++ src/game.js
@@ -16,20 +16,28 @@ let config = {
 		} 
 	}
 };
-
 new Phaser.Game(config);
 
 function preload()
 {
-
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('platform','assets/platform.png');
+    this.load.image('item','assets/diamond.png');
+    this.load.spritesheet('player', 'assets/woof.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create()
 {
+    this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    this.platforms = this.physics.add.staticGroup();   
+    this.platforms.create(400, 400, 'platform').setScale(2).refreshBody();//suelo
+
+    this.platforms.create(50, 250, 'platform');
+    this.platforms.create(750, 220, 'platform');
 
 }
 
 function update()
 {
-    
+
 }
